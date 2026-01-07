0e22f605e24465cf36018499c2707ded0dfab89a
smileyface-istg <pregenerativetrainedai@gmail.com>
2026-01-07 18:25:53 +0800

    Add files via upload

--- api/oembed.js
+++ api/oembed.js
@@ -1,19 +1,22 @@
 
+const { tracks } = require('./tracks.js');
+
 export default async function handler(req, res) {
-    const { id, title, artist, img, src } = req.query;
+    const { id } = req.query;
+    const track = tracks.find(t => t.id === id) || tracks[0];
     const host = req.headers.host;
     const baseUrl = `https://${host}`;
 
-    const embedUrl = `${baseUrl}/embed/${id}?src=${encodeURIComponent(src)}&title=${encodeURIComponent(title)}&artist=${encodeURIComponent(artist)}&img=${encodeURIComponent(img)}`;
+    const embedUrl = `${baseUrl}/embed/${track.id}?src=${encodeURIComponent(track.src)}&title=${encodeURIComponent(track.title)}&artist=${encodeURIComponent(track.artist)}&img=${encodeURIComponent(track.img)}`;
 
     const oembedResponse = {
-        type: "rich",
+        type: "video",
         version: "1.0",
         provider_name: "JaMusic v2",
         provider_url: baseUrl,
-        title: title || "Music Track",
-        author_name: artist || "Unknown Artist",
-        thumbnail_url: img || "",
+        title: track.title,
+        author_name: track.artist,
+        thumbnail_url: track.img,
         thumbnail_width: 512,
         thumbnail_height: 512,
         html: `<iframe src="${embedUrl}" width="500" height="150" frameborder="0" scrolling="no" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
